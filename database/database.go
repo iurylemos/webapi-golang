f3de4b18a248d735bd581757d6c20f4c37ebc7a1
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-04 18:07:34 -0300

    added docker and created connection with database and created  models, controllers using gorm

---
+++ database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func startDB() {
+	str := "host=localhost port=25432 user=admin dbname=books sslmode=disable password=123456"
+
+	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	db = database
+
+	config, _ := db.DB()
+
+	config.SetMaxIdleConns(10)
+	config.SetMaxIdleConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+}
+
+// Method to get db and create only instance
+func GetDataBase() *gorm.DB {
+	return db
+}
