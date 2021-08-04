e0ae5d789aac99aec4b06b2e2413716c92ad6951
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-04 17:46:42 -0300

    create api rest and return values to endpoint books

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/iurylemos/webapi-golang/server"
+
 func main() {
-	
+	server := server.NewServer()
+
+	server.Run()
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,11 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iurylemos/webapi-golang/server/routes"
+)
 
 // Struct to gin create server
 type Server struct {
@@ -18,5 +23,8 @@ func NewServer() Server {
 
 // Method to run server
 func (s *Server) Run() {
+	router := routes.ConfigRoutes(s.server)
 
+	log.Print("server is running at port: ", s.port)
+	log.Fatal(router.Run(":" + s.port))
 }
