cd90efb4bc948bca29c193b004ef3af7ba449afe
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-04 18:10:51 -0300

    adding model to migrations and handling function to cameCase and inserting in main

--- database/database.go
+++ database/database.go
@@ -10,7 +10,7 @@ import (
 
 var db *gorm.DB
 
-func startDB() {
+func StartDB() {
 	str := "host=localhost port=25432 user=admin dbname=books sslmode=disable password=123456"
 
 	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/iurylemos/webapi-golang/server"
+import (
+	"github.com/iurylemos/webapi-golang/database"
+	"github.com/iurylemos/webapi-golang/server"
+)
 
 func main() {
+
+	database.StartDB()
+
 	server := server.NewServer()
 
 	server.Run()
