3fc1c20abdfa47c4b00410cd49a537f8f82163a9
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-04 18:17:19 -0300

    create first endpoint to get book by id

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -1,9 +1,42 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/iurylemos/webapi-golang/database"
+	"github.com/iurylemos/webapi-golang/models"
+)
 
 func ShowBook(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	// c.JSON(200, gin.H{
+	// 	"value": "ok",
+	// })
+
+	id := c.Param("id")
+
+	// transform string to int
+	newId, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+
+		return
+	}
+	db := database.GetDataBase()
+
+	var book models.Book
+
+	err = db.First(&book, newId).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot find book" + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
 }
