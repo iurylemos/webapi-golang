bf8aff9d63a48696a20d2174892eb1a26db897e9
Claudio Iury Lemos Maia - ILE <iury.lemos@iteris.local>
2021-08-08 00:59:41 -0300

    added middleware to authetication in router the books

--- server/routes/router.go
+++ server/routes/router.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/iurylemos/webapi-golang/controllers"
+	"github.com/iurylemos/webapi-golang/server/middlewares"
 )
 
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 	main := router.Group("api/v1")
 	{
-		books := main.Group("books")
+		books := main.Group("books", middlewares.Auth())
 		{
 			books.GET("/:id", controllers.ShowBook)
 			books.PUT("/:id", controllers.UpdateBook)
